c7b97806d4d9486de47a4b07331a550ddf9c4fab
Gustavo Zambonin <gzmbnn@gmail.com>
2017-08-08 21:52:28 -0300

    panning and zooming working

--- main.cpp
+++ main.cpp
@@ -8,18 +8,22 @@ static cairo_surface_t *surface = nullptr;
 GtkBuilder *builder;
 GtkWidget *drawing_area, *window_widget, *viewport, *draw_widget;
 
-std::list<coord> split(const char* input) {
+std::list<drawable> master_list;
+window w;
+
+std::list<coord> split(const char *input) {
   std::list<coord> c;
   std::list<double> tmp;
 
   std::istringstream iss1(input);
   std::string s1, s2;
 
-  while(getline(iss1, s1, ' ')) {
+  while (getline(iss1, s1, ' ')) {
     std::istringstream iss2(s1);
-    while(getline(iss2, s2, ';')) {
+    while (getline(iss2, s2, ';')) {
       tmp.push_back(std::stod(s2));
     }
+    // TODO funciona com um elemento só
     c.push_back(coord(tmp.front(), tmp.back()));
     tmp.clear();
   }
@@ -68,6 +72,30 @@ extern "C" G_MODULE_EXPORT void btn_w2cancel_clk() {
   gtk_widget_destroy(GTK_WIDGET(draw_widget));
 }
 
+coord viewport_tr(coord &c) {
+  double xvp = ((c.x - w.xmin) / (w.xmax - w.xmin)) * (200 - -200);
+  double yvp = (1 - ((c.y - w.ymin) / (w.ymax - w.ymin))) * (200 - -200);
+  return coord(xvp, yvp);
+}
+
+void update_roleplay() {
+  clear_surface();
+  cairo_t *cr = cairo_create(surface);
+  cairo_set_line_width(cr, 2);
+  cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
+
+  for (auto &i : master_list) {
+    std::list<coord> tmp;
+    for (auto &j : i.clist) {
+      tmp.push_back(viewport_tr(j));
+    }
+    drawable d(std::string("copy"), cr, tmp);
+    d.draw();
+    tmp.clear();
+    gtk_widget_queue_draw(window_widget);
+  }
+}
+
 extern "C" G_MODULE_EXPORT void btn_draw_figure_clk(GtkWidget *widget,
                                                     GtkWidget *entry) {
   std::list<coord> c = split(gtk_entry_get_text(GTK_ENTRY(entry)));
@@ -78,21 +106,43 @@ extern "C" G_MODULE_EXPORT void btn_draw_figure_clk(GtkWidget *widget,
   // TODO add entry for name
   drawable p(std::string("placeholder"), cr, c);
 
-  // TODO add drawable master list
-  // master_list.push_back(p);
+  master_list.push_back(p);
 
-  p.draw();
+  update_roleplay();
 
-  gtk_widget_queue_draw(window_widget);
   gtk_widget_destroy(draw_widget);
 }
 
-extern "C" G_MODULE_EXPORT void btn_pan_up_clk() {}
-extern "C" G_MODULE_EXPORT void btn_pan_left_clk() {}
-extern "C" G_MODULE_EXPORT void btn_pan_right_clk() {}
-extern "C" G_MODULE_EXPORT void btn_pan_down_clk() {}
-extern "C" G_MODULE_EXPORT void btn_zoom_in_clk() {}
-extern "C" G_MODULE_EXPORT void btn_zoom_out_clk() {}
+extern "C" G_MODULE_EXPORT void btn_pan_up_clk() {
+  w.set_limits(0, 0, 10, 10);
+  update_roleplay();
+}
+
+extern "C" G_MODULE_EXPORT void btn_pan_left_clk() {
+  w.set_limits(-10, -10, 0, 0);
+  update_roleplay();
+}
+
+extern "C" G_MODULE_EXPORT void btn_pan_right_clk() {
+  w.set_limits(10, 10, 0, 0);
+  update_roleplay();
+}
+
+extern "C" G_MODULE_EXPORT void btn_pan_down_clk() {
+  w.set_limits(0, 0, -10, -10);
+  update_roleplay();
+}
+
+extern "C" G_MODULE_EXPORT void btn_zoom_out_clk() {
+  w.zoom(1.2, 1.2, 1.2, 1.2);
+  update_roleplay();
+}
+
+extern "C" G_MODULE_EXPORT void btn_zoom_in_clk() {
+  w.zoom(0.8, 0.8, 0.8, 0.8);
+  update_roleplay();
+}
+
 extern "C" G_MODULE_EXPORT void btn_exit_clk() { gtk_main_quit(); }
 
 int main(int argc, char *argv[]) {
