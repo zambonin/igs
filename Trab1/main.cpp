0c74b80d6c86c0cc2592381661d165fdaf33884b
Gustavo Zambonin <gzmbnn@gmail.com>
2017-08-10 09:55:10 -0300

    object names added

--- Trab1/main.cpp
+++ Trab1/main.cpp
@@ -3,9 +3,10 @@
 #include <sstream>
 #include "structures.hpp"
 
-static cairo_surface_t *surface = NULL;
-GtkWidget *drawing_area;
-GtkWidget *window_widget;
+static cairo_surface_t *surface = nullptr;
+
+GtkWidget *drawing_area, *window_widget;
+GtkBuilder *builder;
 
 std::list<drawable> master_list;
 window w;
@@ -31,56 +32,62 @@ std::list<coord> split(const char* input) {
 
 }
 
-/*Clear the surface, removing the scribbles*/
-static void clear_surface (){
-    cairo_t *cr;
+static void clear_surface() {
 
-    cr = cairo_create (surface);
+  cairo_t *cr = cairo_create(surface);
+  cairo_set_source_rgb(cr, 1, 1, 1);
+  cairo_paint(cr);
+  cairo_destroy(cr);
 
-    cairo_set_source_rgb (cr, 1, 1, 1);
-    cairo_paint (cr);
-
-    cairo_destroy (cr);
 }
 
-/*Creates the surface*/
-static gboolean configure_event_cb (GtkWidget *widget, GdkEventConfigure *event, gpointer data){
-    if (surface)
-        cairo_surface_destroy (surface);
+static gboolean configure_event_cb(GtkWidget *widget,
+    GdkEventConfigure *event, gpointer data) {
 
-    surface = gdk_window_create_similar_surface (gtk_widget_get_window (widget),
-            CAIRO_CONTENT_COLOR,
-            gtk_widget_get_allocated_width (widget),
-            gtk_widget_get_allocated_height (widget));
-    clear_surface ();
-    return TRUE;
+  if (surface) {
+    cairo_surface_destroy (surface);
+  }
+
+  surface = gdk_window_create_similar_surface(gtk_widget_get_window(widget),
+      CAIRO_CONTENT_COLOR, gtk_widget_get_allocated_width (widget),
+      gtk_widget_get_allocated_height (widget));
+  clear_surface();
+
+  return TRUE;
+
 }
 
-/* Redraw the screen from the surface */
-static gboolean draw_cb (GtkWidget *widget, cairo_t   *cr,  gpointer   data){
-    cairo_set_source_surface (cr, surface, 0, 0);
-    cairo_paint (cr);
+static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data) {
 
-    return FALSE;
+  cairo_set_source_surface(cr, surface, 0, 0);
+  cairo_paint(cr);
+
+  return FALSE;
+
 }
 
 coord viewport_tr(coord& c) {
-  int width;
-  int height;
+
+  int width, height;
   gtk_widget_get_size_request(drawing_area, &width, &height);
+
   double xvp = ((c.x - w.xmin) / (w.xmax - w.xmin)) * (width - 0);
   double yvp = (1 - ((c.y - w.ymin) / (w.ymax - w.ymin))) * (height - 0);
+
   return coord(xvp, yvp);
+
 }
 
-void update_roleplay() {
+void update() {
+
   clear_surface();
+  std::list<coord> tmp;
+
   cairo_t *cr = cairo_create(surface);
   cairo_set_line_width(cr, 2);
   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
-  for (auto& i : master_list) {
 
-    std::list<coord> tmp;
+  for (auto& i : master_list) {
     for (auto& j : i.clist) {
       tmp.push_back(viewport_tr(j));
     }
@@ -89,80 +96,110 @@ void update_roleplay() {
     tmp.clear();
     gtk_widget_queue_draw(window_widget);
   }
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_draw_figure_clk(GtkWidget *widget, GtkWidget *entry) {
-  std::list<coord> c = split(gtk_entry_get_text(GTK_ENTRY(entry)));
+extern "C" G_MODULE_EXPORT void btn_draw_figure_clk() {
+
+  GtkEntry *name = GTK_ENTRY(gtk_builder_get_object(builder, "name")),
+           *coor = GTK_ENTRY(gtk_builder_get_object(builder, "coord"));
+
   cairo_t *cr = cairo_create(surface);
   cairo_set_line_width(cr, 2);
   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
-  // TODO add entry for name
-  drawable p(std::string("placeholder"), cr, c);
 
+  std::list<coord> c = split(gtk_entry_get_text(coor));
+  drawable p(std::string(gtk_entry_get_text(name)), cr, c);
   master_list.push_back(p);
 
-  update_roleplay();
+  update();
 
-  gtk_entry_set_text(GTK_ENTRY(entry), "");
+  gtk_entry_set_text(name, "");
+  gtk_entry_set_text(coor, "");
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_pan_up_clk(GtkWidget *widget, GtkWidget *entry) {
-  const double rate = std::atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+extern "C" G_MODULE_EXPORT void btn_pan_up_clk(
+    GtkWidget *widget, GtkWidget *entry) {
+
+  const double rate = std::stod(gtk_entry_get_text(GTK_ENTRY(entry)));
   w.set_limits(0, 0, rate, rate);
-  update_roleplay();
+  update();
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_pan_left_clk(GtkWidget *widget, GtkWidget *entry) {
-  const double rate = std::atof(gtk_entry_get_text(GTK_ENTRY(entry)));
-  w.set_limits(-1*rate, -1*rate, 0, 0);
-  update_roleplay();
+extern "C" G_MODULE_EXPORT void btn_pan_left_clk(
+    GtkWidget *widget, GtkWidget *entry) {
+
+  const double rate = std::stod(gtk_entry_get_text(GTK_ENTRY(entry)));
+  w.set_limits(-1 * rate, -1 * rate, 0, 0);
+  update();
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_pan_right_clk(GtkWidget *widget, GtkWidget *entry) {
-  const double rate = std::atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+extern "C" G_MODULE_EXPORT void btn_pan_right_clk(
+    GtkWidget *widget, GtkWidget *entry) {
+
+  const double rate = std::stod(gtk_entry_get_text(GTK_ENTRY(entry)));
   w.set_limits(rate, rate, 0, 0);
-  update_roleplay();
+  update();
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_pan_down_clk(GtkWidget *widget, GtkWidget *entry) {
-  const double rate = std::atof(gtk_entry_get_text(GTK_ENTRY(entry)));
-  w.set_limits(0, 0, -1*rate, -1*rate);
-  update_roleplay();
+extern "C" G_MODULE_EXPORT void btn_pan_down_clk(
+    GtkWidget *widget, GtkWidget *entry) {
+
+  const double rate = std::stod(gtk_entry_get_text(GTK_ENTRY(entry)));
+  w.set_limits(0, 0, -1 * rate, -1 * rate);
+  update();
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_zoom_out_clk(GtkWidget *widget, GtkWidget *entry) {
-  const double rate = std::atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+extern "C" G_MODULE_EXPORT void btn_zoom_out_clk(
+    GtkWidget *widget, GtkWidget *entry) {
+
+  const double rate = std::stod(gtk_entry_get_text(GTK_ENTRY(entry)));
   w.zoom(1 + rate);
-  update_roleplay();
+  update();
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_zoom_in_clk(GtkWidget *widget, GtkWidget *entry) {
-  const double rate = std::atof(gtk_entry_get_text(GTK_ENTRY(entry)));
+extern "C" G_MODULE_EXPORT void btn_zoom_in_clk(
+    GtkWidget *widget, GtkWidget *entry) {
+
+  const double rate = std::stod(gtk_entry_get_text(GTK_ENTRY(entry)));
   w.zoom(1 - rate);
-  update_roleplay();
+  update();
+
 }
 
-extern "C" G_MODULE_EXPORT void btn_exit_clk(){
+extern "C" G_MODULE_EXPORT void btn_exit_clk() {
   gtk_main_quit();
 }
 
-extern "C" G_MODULE_EXPORT void btn_clear_clk(){
+extern "C" G_MODULE_EXPORT void btn_clear_clk() {
   clear_surface();
+  master_list.clear();
   gtk_widget_queue_draw(window_widget);
 }
 
-int main(int argc, char *argv[]){
-    GtkBuilder  *gtkBuilder;
-    gtk_init(&argc, &argv);
+int main(int argc, char *argv[]) {
 
-    gtkBuilder = gtk_builder_new_from_file("src/window.glade");
-    window_widget = GTK_WIDGET( gtk_builder_get_object( GTK_BUILDER(gtkBuilder), "main_window") );
-    drawing_area = GTK_WIDGET( gtk_builder_get_object( GTK_BUILDER(gtkBuilder), "drawing_area") );
+  gtk_init(&argc, &argv);
 
-    g_signal_connect (drawing_area, "draw", G_CALLBACK (draw_cb), NULL);
-    g_signal_connect (drawing_area,"configure-event", G_CALLBACK (configure_event_cb), NULL);
-    gtk_builder_connect_signals(gtkBuilder, NULL);
-    gtk_widget_show_all(window_widget);
-    gtk_main ();
-    return 0;
+  builder = gtk_builder_new_from_file("src/window.glade");
+  window_widget = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+  drawing_area = GTK_WIDGET(gtk_builder_get_object(builder, "drawing_area"));
+
+  g_signal_connect(drawing_area, "draw",
+      G_CALLBACK(draw_cb), nullptr);
+  g_signal_connect(drawing_area, "configure-event",
+      G_CALLBACK(configure_event_cb), nullptr);
+
+  gtk_builder_connect_signals(builder, nullptr);
+  gtk_widget_show_all(window_widget);
+  gtk_main();
+
+  return 0;
+
 }
